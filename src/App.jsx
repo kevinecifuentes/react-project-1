88a5d9cb0cbf565bdcd5a33b38264adf297fca06
Kevin Cifuentes <kevmusic25@gmail.com>
2023-05-21 11:18:05 -0500

    starting project

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <main className='app'>
+
+        <section className='app_containter'>
+
+          <h1>Fortune Coockies</h1>
+
+          <div className="image_container">
+           <img src="/images/rectangle-1.png" alt="rectangle"/>
+          </div>
+          
+        </section>
+
+      </main>
+    </>
+  )
+}
+
+export default App
