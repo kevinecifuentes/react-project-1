29ddd9ed1b02823e392a78026a47f6c34d6a2c1c
Kevin Cifuentes <kevmusic25@gmail.com>
2023-05-24 00:44:08 -0500

    ended project 1.1

--- src/components/FooterBox.jsx
+++ src/components/FooterBox.jsx
@@ -2,8 +2,8 @@ const FooterBox = ({ author }) => {
   return (
     <footer className="footer">
       <div className="footer__container">
-        <h3>
-          <b>Author: </b>
+        <h3 className="h3">
+          <b className="author">Author: </b>
           {author}
         </h3>
       </div>
